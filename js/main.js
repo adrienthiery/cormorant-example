e3970032ba20f95582c83b2ae7eeea990c8f7604
Adrien Thiery <adrien.thiery@gmail.com>
2015-07-27 12:36:54 +0200

    Adapted example to minified version of cormorant

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
-React = require(['../bower_components/react/react', '../cormorant/tools', '../cormorant/main'], function(React,
-tools, getReactComponent){
-  var req = new XMLHttpRequest();
-  req.open( "GET", 'data/test-owns.json' );
-  req.send();
-  setTimeout( function(){
-    var myJSON = req.response;
-    myJSON = JSON.parse( myJSON );
-    var component = getReactComponent(myJSON);
-    React.render(
-      React.createElement(component, {props: myJSON}),
-      document.getElementById('content')
-    );
-  }, 500);
-});
+var req = new XMLHttpRequest();
+req.open( "GET", 'data/test-owns.json' );
+req.send();
+setTimeout( function(){
+  var myJSON = req.response;
+  myJSON = JSON.parse( myJSON );
+  var component = cormorant.getComponent(myJSON);
+  React.render(
+    React.createElement(component, {props: myJSON}),
+    document.getElementById('content')
+  );
+}, 500);
